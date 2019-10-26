f0ff71f9f9d703d8db7307f088884757d3465e85
mertkahyaoglu <mertkahyaoglu93@gmail.com>
2019-10-26 15:00:33 +0300

    Extract ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:booklines/theme.dart' as MainTheme;
 import 'package:flutter/material.dart';
 import 'package:booklines/screens/home.dart';
 import 'package:booklines/screens/book.dart';
@@ -5,37 +6,18 @@ import 'package:booklines/screens/book.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-    final theme = new ThemeData(
-        primaryColor: Color.fromRGBO(85, 77, 86, 1.0),
-        buttonTheme: ButtonThemeData(
-            buttonColor: Color.fromRGBO(65, 155, 249, 1.0),
-            textTheme: ButtonTextTheme.primary,
-            shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(2.0),),
-            padding: EdgeInsets.symmetric(vertical: 4, horizontal: 12)
-        ),
-        cardTheme: CardTheme(
-            color: Colors.white,
-            elevation: 0,
-            margin: new EdgeInsets.only(bottom: 12),
-            shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(2.0)),
-            clipBehavior: Clip.antiAlias,
-
-        )
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: MainTheme.theme,
+      initialRoute: '/',
+      routes: {
+        // When navigating to the "/" route, build the FirstScreen widget.
+        '/': (context) => HomePage(title: "Home"),
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/book': (context) => BookPage(),
+      },
     );
-
-    // This widget is the root of your application.
-    @override
-    Widget build(BuildContext context) {
-        return new MaterialApp(
-        title: 'Flutter Demo',
-        theme: theme,
-        initialRoute: '/',
-        routes: {
-            // When navigating to the "/" route, build the FirstScreen widget.
-            '/': (context) => HomePage(title: "Home"),
-            // When navigating to the "/second" route, build the SecondScreen widget.
-            '/book': (context) => BookPage(),
-        },
-        );
-    }
+  }
 }
---
+++ lib/theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const primaryColor = Color.fromRGBO(65, 155, 249, 1.0);
+  static const textColor = Color.fromRGBO(85, 77, 86, 1.0);
+  static const borderColor = Color.fromRGBO(237, 236, 237, 1.0);
+  static const backgroundColor = Color.fromRGBO(251, 251, 251, 1.0);
+}
+
+final theme = new ThemeData(
+    primaryTextTheme:
+        TextTheme(title: TextStyle(color: ThemeColors.textColor)),
+    primaryColor: ThemeColors.primaryColor,
+    buttonTheme: ButtonThemeData(
+        textTheme: ButtonTextTheme.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: new BorderRadius.circular(2.0),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 4, horizontal: 12)),
+    cardTheme: CardTheme(
+      color: Colors.white,
+      elevation: 0.2,
+      margin: new EdgeInsets.only(bottom: 12),
+      shape: RoundedRectangleBorder(
+          side: BorderSide(width: 1, color: ThemeColors.borderColor),
+          borderRadius: new BorderRadius.circular(2.0)),
+      clipBehavior: Clip.antiAlias,
+    ),
+    appBarTheme: AppBarTheme(
+        iconTheme: IconThemeData(color: ThemeColors.primaryColor),
+        actionsIconTheme:
+            IconThemeData(color: ThemeColors.primaryColor),
+        color: ThemeColors.backgroundColor,
+        textTheme: TextTheme(),
+        elevation: 1));
